0cdae3b2a62f61b2ee957054de1f96343b96d643
wode331 <752494484@qq.com>
2020-08-07 19:50:21 +0800

    TeatA版本3

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,9 +1,12 @@
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("322");
     }
     public void run(){
         System.out.println("run");
     }
+    public void start(){
+        System.out.println("haha");
+    }
+
 }
